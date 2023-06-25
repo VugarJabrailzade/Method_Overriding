0797029df9a8b468ada5c7cbfdba0559d47be2f6
Vugar Jabrailzade <vugarej@code.edu.az>
2023-06-25 14:22:09 +0400

    task completed succesfuly

---
+++ Method_Overriding/Method_Overriding/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection.Emit;
+
+
+namespace Overriding
+{
+    sealed class Person 
+    {
+        public string  Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+
+        public Person(string name, string surname, int age, string weight, string height)
+        {
+            Name = name;
+            SurName = surname;
+            Age = age;
+            Weight = weight;
+            Height = height;
+
+        }
+        public override string ToString()
+        {
+            return "Person:" + Name + " " + SurName + " " + Age + " " + Weight + " " + Height;
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Vugar","Jabrailzade",25,"70kg","178sm");
+
+
+            Console.WriteLine(person);
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+}
