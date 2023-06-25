02d4fa9761b575ce592dfbf64568d8be8b7d0d00
Vugar Jabrailzade <vugarej@code.edu.az>
2023-06-25 14:34:34 +0400

    Change completed

--- Method_Overriding/Method_Overriding/Program.cs
+++ Method_Overriding/Method_Overriding/Program.cs
@@ -4,15 +4,15 @@ using System.Reflection.Emit;
 
 namespace Overriding
 {
-    sealed class Person 
+     sealed class  Person 
     {
         public string  Name { get; set; }
         public string SurName { get; set; }
-        public int Age { get; set; }
+        public string Age { get; set; }
         public string Weight { get; set; }
         public string Height { get; set; }
 
-        public Person(string name, string surname, int age, string weight, string height)
+        public Person(string name, string surname, string age, string weight, string height)
         {
             Name = name;
             SurName = surname;
@@ -34,7 +34,7 @@ namespace Overriding
     {
         static void Main(string[] args)
         {
-            var person = new Person("Vugar","Jabrailzade",25,"70kg","178sm");
+            var person = new Person("Vugar","Jabrailzade","25","70kg","178sm");
 
 
             Console.WriteLine(person);
